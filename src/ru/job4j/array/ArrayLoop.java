2a13365a317de9887a203eb22e82aa6ebdaa81e6
bogdanove <anoth3rx@gmail.com>
2020-11-01 20:56:14 +0300

    6.0.3.1. Массивы и цикл for-earch [#333581]

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -6,8 +6,8 @@ public class ArrayLoop {
         for (int i = 0; i < func.length; i++) {
             func[i] = i * 2 + 3;
         }
-        for (int index = 0; index < func.length; index++) {
-            System.out.println(func[index]);
+        for (int index : func) {
+            System.out.println(index);
         }
     }
 }
